2a592399b5e24830fc9534043b2121b9ccf72947
John Oduya <oduyajohn66@gmail.com>
2024-03-06 17:14:02 +0300

    new-portfolio

---
+++ static/script.js
@@ -0,0 +1,22 @@
+
+
+function togglemenu(){
+	const menu = document.querySelector(".menu-links");
+	const icon = document.querySelector(".ham-icon");
+	menu.classList.toggle("open");
+	icon.classList.toggle("open");
+}
+
+// let typed = new Typed('auto-type',
+// 	{
+// 	strings: [],
+// 	typeSpeed: 150,
+// 	backSpeed: 150,
+// 	loop: true,
+// })
+
+// const typed = new Typed('#typed-text', {
+//     strings: ['Hello, World!', 'Welcome to Typed.js!'],
+//     typeSpeed: 50,
+//     loop: true
+//   });
